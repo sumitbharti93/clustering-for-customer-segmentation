820fc466cde291d111550e1e13d9877a1c64bcd3
sumitbharti93 <sumitbharti93@gmail.com>
2023-02-09 23:04:23 +0530

    workflow file updated

--- app.py
+++ app.py
@@ -176,5 +176,5 @@ def render_log_dir(req_path):
 
 if __name__ == "__main__":
 
-    # app.run(host='0.0.0.0', port =8080) # for deployment run
-    app.run(host = '127.0.0.1',port = 8080, debug=True ) # for local run 
+    app.run(host='0.0.0.0', port =8080) # for deployment run
+    #app.run(host = '127.0.0.1',port = 8080, debug=True ) # for local run 
